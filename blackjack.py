2ef474213487bbd6d88ae224f07d16b207cae5e7
Ron Kaptein <ronkaptein@upcmail.nl>
2018-08-02 19:24:32 +0200

    Initial commit

--- blackjack.py
+++ blackjack.py
@@ -121,6 +121,11 @@ def new_game():
 def shuffle():
     random.shuffle(deck)
 
+
+def ron():
+    print("Ron says hello")
+
+
 mainWindow = tkinter.Tk()
 
 # Set up the screen and frames for the dealer and player
